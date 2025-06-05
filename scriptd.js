bd58707dcb6207a00002b23cc93e1f7cc44cca79
Sandhya3158 <sandhyamadhunagula08@gmail.com>
2025-06-05 23:09:46 +0530

    Add files via upload

---
+++ scriptd.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+  const navLinks = document.getElementById('navLinks');
+  navLinks.classList.toggle('active');
+}
+/* View collect button 
+<button class="vcollect" onclick="window.location.href='collection.html'">✨ View Collection</button>
+*/
